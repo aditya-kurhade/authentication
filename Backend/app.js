4bf63fc5a8cf7c8cf3fb9289a5662db591f8bc7f
Aditya Kurhade <adityakurhade1000@gmail.com>
2025-12-10 22:41:08 +0530

    initialize authentication backend with express server setup

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Authentication Backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
